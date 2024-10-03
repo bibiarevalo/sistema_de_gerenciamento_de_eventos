8bdabf673d1bd059f99d5ab4501dd355f2f1869a
Beatriz Arevalo <biaarevalofreitas@gmail.com>
2024-10-03 20:41:55 -0300

    add paginas de navegação

--- app-blueTicket/src/main.jsx
+++ app-blueTicket/src/main.jsx
@@ -1,9 +1,16 @@
-
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Cadastro from './pages/Cadastro'
+import Detalhes from './pages/Detalhes'
+import ListEvent from './pages/ListEvent'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
+const paginas = createBrowserRouter([
+  { path: '/', element: <Cadastro /> },
+  { path: '/listEvent/:id', element: <ListEvent /> },
+  { path: '/detalhes/:id', element: <Detalhes /> },
+])
 
-    <App />
+createRoot(document.getElementById('root')).render(
+  <RouterProvider router={paginas} />,
 )
